ca5d9d9d3a853325becb3c552fb3ed74d22902ae
max <max>
2021-11-03 03:10:23 +0200

    Initial Commit

---
+++ Misha App/Misha_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Misha_AppApp.swift
+//  Misha App
+//
+//  Created by Максим Гарбарт on 03.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Misha_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
